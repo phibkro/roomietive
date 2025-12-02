158f397ef0bc4ffad88b5da3ffab38b3bb4f648a
phibkro <71797726+phibkro@users.noreply.github.com>
2025-12-02 02:20:50 +0100

    feat: :heavy_plus_sign: add drizzle & sqlite

--- apps/native/app/(drawer)/index.tsx
+++ apps/native/app/(drawer)/index.tsx
@@ -1,14 +1,39 @@
 import { Text, View, TouchableOpacity } from "react-native";
 import { Container } from "@/components/container";
 import { Card, Chip, useThemeColor } from "heroui-native";
+import { usersTable } from "@/db/schema";
+import { useEffect, useState } from "react";
+import { db } from "@/db/drizzle";
 
 export default function Home() {
+  const [items, setItems] = useState<typeof usersTable.$inferSelect[] | null>(null);
+
+    useEffect(() => {
+      (async () => {
+      const users = await db.select().from(usersTable);
+      setItems(users);
+    })();
+  }, []);
+
 	return (
 		<Container className="p-6">
 			<View className="py-4 mb-6">
 				<Text className="text-4xl font-bold text-foreground mb-2">
 					BETTER T STACK
 				</Text>
+				{items ? (
+					items.map((item) => (
+						<Card key={item.id} className="mb-4 p-4">
+							<Text className="text-lg font-semibold text-foreground mb-1">
+								{item.name}
+							</Text>
+							<Text className="text-foreground">Age: {item.age}</Text>
+							<Text className="text-foreground">Email: {item.email}</Text>
+						</Card>
+					))
+				) : (
+					<Text className="text-foreground">No items available.</Text>
+				)}
 			</View>
 		</Container>
 	);
--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -5,12 +5,50 @@ import { HeroUINativeProvider } from "heroui-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { AppThemeProvider } from "@/contexts/app-theme-context";
+import { useEffect, useState } from 'react';
+import { usersTable } from '@/db/schema';
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+import migrations from '@/drizzle/migrations';
+import * as SQLite from 'expo-sqlite';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+
+const expo = SQLite.openDatabaseSync('db.db');
+
+const db = drizzle(expo);
 
 export const unstable_settings = {
 	initialRouteName: "(drawer)",
 };
 
 function StackLayout() {
+  const { success, error } = useMigrations(db, migrations);
+  const [items, setItems] = useState<typeof usersTable.$inferSelect[] | null>(null);
+
+  useEffect(() => {
+    if (!success) return;
+
+    (async () => {
+      await db.delete(usersTable);
+
+      await db.insert(usersTable).values([
+        {
+            name: 'John',
+            age: 30,
+            email: 'john@example.com',
+        },
+      ]);
+
+      const users = await db.select().from(usersTable);
+      setItems(users);
+    })();
+  }, [success]);
+
+  	if (error) {
+		console.error(`Migration error: ${error.message}`);
+  	}
+
+	console.info('Items:', items);
+
 	return (
 		<Stack screenOptions={{}}>
 			<Stack.Screen name="(drawer)" options={{ headerShown: false }} />
